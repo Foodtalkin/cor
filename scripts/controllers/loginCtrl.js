a70c8ac0b81e3e1c7d0f54dd2f93043fdb726358
rchshrm16 <rchshrm16@gmail.com>
2016-07-26 19:13:21 +0530

    app dashboard

--- scripts/controllers/loginCtrl.js
+++ scripts/controllers/loginCtrl.js
@@ -2,7 +2,7 @@ app.controller('loginCtrl', ['$scope', 'authFactory', '$location', function($sco
   $scope.doLogin = function(){
     authFactory.doLogin($scope.username, $scope.password, function(response){
       if(response){
-        $location.path('/dashboard');
+        $location.path('/appdashboard');
       }else{
         //Display generic error
         console.log("Le wild login error");
