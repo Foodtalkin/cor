0a3ececf9dd9c8f2f14f67a15ca53b3c600579d7
rchshrm16 <rchshrm16@gmail.com>
2016-09-27 13:50:18 +0530

    mail test

--- api/mail.php
+++ api/mail.php
@@ -2,7 +2,9 @@
 	$to = $_GET['email'];
 	$subject = $_GET['subject'];
 	$message = $_GET['message'];
-	$headers = 'From: richa@foodtalkindia.com' . "\r\n" .
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+	$headers .= 'From: richa@foodtalkindia.com' . "\r\n" .
 			    'Reply-To: richa@foodtalkindia.com' . "\r\n" ;
 	$sender = mail($to, $subject, $message, $headers);
 	echo $sender;
