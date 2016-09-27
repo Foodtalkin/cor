6765fce52df547952727043fe7165bcaf438890f
rchshrm16 <rchshrm16@gmail.com>
2016-09-27 14:32:09 +0530

    mail test

--- api/mail.php
+++ api/mail.php
@@ -5,10 +5,10 @@
 	$to = $data->email;
 	$subject = $data->subject;
 	$message = $data->message;
-	// $headers  = 'MIME-Version: 1.0' . "\r\n";
- //    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-	// $headers .= 'From: richa@foodtalkindia.com' . "\r\n" .
-	// 		    'Reply-To: richa@foodtalkindia.com' . "\r\n" ;
-	// $sender = mail($to, $subject, $message, $headers);
-	echo $to;
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+	$headers .= 'From: richa@foodtalkindia.com' . "\r\n" .
+			    'Reply-To: richa@foodtalkindia.com' . "\r\n" ;
+	$sender = mail($to, $subject, $message, $headers);
+	echo $sender;
 ?>
