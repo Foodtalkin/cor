537fc0a1e481e42d6ca0e6b6c2787414f91a5616
rchshrm16 <rchshrm16@gmail.com>
2016-09-27 14:10:52 +0530

    mail test

--- api/mail.php
+++ api/mail.php
@@ -1,7 +1,10 @@
 <?php
-	$to = $_POST['email'];
-	$subject = $_POST['subject'];
-	$message = $_POST['message'];
+	$post_date = file_get_contents(“php://input”);
+	$data = json_decode($post_date);
+
+	$to = $data->email;
+	$subject = $data->subject;
+	$message = $data->message;
 	// $headers  = 'MIME-Version: 1.0' . "\r\n";
  //    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
 	// $headers .= 'From: richa@foodtalkindia.com' . "\r\n" .
