d6915f4741ae317f8055d63f5e44a53d23d1ace4
rchshrm16 <rchshrm16@gmail.com>
2016-10-05 16:07:06 +0530

    #4

--- api/sendgridapi.php
+++ api/sendgridapi.php
@@ -8,11 +8,6 @@ $curl = curl_init();
 
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://api.sendgrid.com/v3/mail/send",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
   CURLOPT_POSTFIELDS => $mbody,
   CURLOPT_HTTPHEADER => array(
